1eb859afb641a440edead6e71b278f9a861c5ffa
TrungDev <vantrungarsenal@gmail.com>
2023-04-26 12:12:46 +0700

    created app 26/04/2023 and designe UI

---
+++ list_song.js
@@ -0,0 +1,29 @@
+// danh sach cac bai hat
+const songs = [
+    {   id: 1,
+        name: 'Heavydirtysoul',
+        singer:'Issac',
+        time: '3:54',
+        path: './music/mussic1.mp3',
+        liked: [0,1],
+        image:  './img/img1.png'
+    },
+    {   id: 2,
+        name: 'StressedOut',
+        singer:'Issac',
+        time: '3:54',
+        path: './music/mussic1.mp3',
+        liked: [0,1],
+        image:  './img/img1.png'
+    },
+    {   id: 1,
+        name: 'Fairy Local',
+        singer:'Issac',
+        time: '3:54',
+        path: './music/mussic1.mp3',
+        liked: [0,1],
+        image:  './img/img1.png'
+    }
+]
+
+export default songs
